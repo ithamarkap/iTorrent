ace0d73df834c54393696239de5686a1863c4a25
Ithamar Kaplan <63201859+ithamarkap@users.noreply.github.com>
2026-05-03 17:26:54 +0300

    Added custom icon (will be fixed later)

--- gui/electron/main.js
+++ gui/electron/main.js
@@ -63,6 +63,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        icon: path.join(__dirname, '..', 'icon.ico'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: false,
